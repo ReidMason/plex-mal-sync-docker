2ba063bd2ee50217363a485929a0506cec360d76
Reid Mason <maddogshain132@gmail.com>
2019-07-05 07:58:26 +0100

    Removed dangling \r and \n in log strings

--- plex_connection.py
+++ plex_connection.py
@@ -7,90 +7,90 @@ from utils import log
 
 
 class PlexConnection:
-  def __init__(self, server_url, server_token):
-    """ Connects to plex server with the given url and token.
+    def __init__(self, server_url, server_token):
+        """ Connects to plex server with the given url and token.
 
-    :param server_url: The url to the target plex server.
-    :param server_token: The token for the target server.
-    """
-    log('\nConnecting to plex server', Fore.CYAN)
-    self.server = PlexServer(server_url, server_token)
-    log('\rConnected to plex server', Fore.GREEN)
-    self.tvdb_myanimelist_mapping = utils.load_json('data/tvdb_myanimelist_mapping.json')
-    self.tvdb_anidb_mapping = utils.get_tvdb_anidb_mapping()
+        :param server_url: The url to the target plex server.
+        :param server_token: The token for the target server.
+        """
+        log('Connecting to plex server', Fore.CYAN)
+        self.server = PlexServer(server_url, server_token)
+        log('Connected to plex server', Fore.GREEN)
+        self.tvdb_myanimelist_mapping = utils.load_json('data/tvdb_myanimelist_mapping.json')
+        self.tvdb_anidb_mapping = utils.get_tvdb_anidb_mapping()
 
-  def get_shows(self, library):
-    """ Gets all the shows in a given library.
+    def get_shows(self, library):
+        """ Gets all the shows in a given library.
 
-    :param library: The name of the target library.
-    :return: A list of Show objects from the target library.
-    """
-    return self.server.library.section(library).all()
+        :param library: The name of the target library.
+        :return: A list of Show objects from the target library.
+        """
+        return self.server.library.section(library).all()
 
-  def get_watched_episodes(self, library):
-    """ Get a dictionary of all shows in a library with the episodes watched in each season of the show
+    def get_watched_episodes(self, library):
+        """ Get a dictionary of all shows in a library with the episodes watched in each season of the show
 
-    :param library: The name of the target library.
-    :return: A dictionary with the episodes watched for each season in a show.
-    """
-    data = {}
-    for show in self.get_shows(library):
-      # Get all seasons excluding specials
-      seasons = list(filter(lambda x: 'Specials' not in x.title, show.seasons()))
-      data[show.title] = {s.seasonNumber: len([x for x in s if x.isWatched]) for s in seasons}
+        :param library: The name of the target library.
+        :return: A dictionary with the episodes watched for each season in a show.
+        """
+        data = {}
+        for show in self.get_shows(library):
+            # Get all seasons excluding specials
+            seasons = list(filter(lambda x: 'Specials' not in x.title, show.seasons()))
+            data[show.title] = {s.seasonNumber: len([x for x in s if x.isWatched]) for s in seasons}
 
-    return data
+        return data
 
-  def get_show_tvdbid(self, show):
-    """ Gets the tvdb id of the passed show.
+    def get_show_tvdbid(self, show):
+        """ Gets the tvdb id of the passed show.
 
-    :param show: A Show object to get the tvdbid from.
-    :return: The tvdb id for the target Show.
-    """
-    return show.guid.split('//', 1)[1].rstrip('?lang=en')
+        :param show: A Show object to get the tvdbid from.
+        :return: The tvdb id for the target Show.
+        """
+        return show.guid.split('//', 1)[1].rstrip('?lang=en')
 
-  def create_mapping(self, show, season, driver):
-    """ Creates a mapping from tvdb id to the mal id.
+    def create_mapping(self, show, season, driver):
+        """ Creates a mapping from tvdb id to the mal id.
 
-    Using the tvdb to anidb mapping the corresponding anidb id is found.
-    If there is no tvdb to anidb mapping it is added to the error log for manual entry.
-    The anidb page is loaded and then the myanimelist id is scraped from there.
+        Using the tvdb to anidb mapping the corresponding anidb id is found.
+        If there is no tvdb to anidb mapping it is added to the error log for manual entry.
+        The anidb page is loaded and then the myanimelist id is scraped from there.
 
-    :param show: Show object for the show to map.
-    :param season: The season number to create the mapping for.
-    :param driver: A chromedriver to get the myanimelist link from anidb.
-    """
-    tvdbid = self.get_show_tvdbid(show)
-    anidb_id = utils.find_anidb_id(self.tvdb_anidb_mapping, tvdbid, season)
-    myanimelist_id = utils.get_myanimelist_id(anidb_id, driver)
+        :param show: Show object for the show to map.
+        :param season: The season number to create the mapping for.
+        :param driver: A chromedriver to get the myanimelist link from anidb.
+        """
+        tvdbid = self.get_show_tvdbid(show)
+        anidb_id = utils.find_anidb_id(self.tvdb_anidb_mapping, tvdbid, season)
+        myanimelist_id = utils.get_myanimelist_id(anidb_id, driver)
 
-    # If the myanimelist id could not be found then it needs to logged so I can add the mapping manually
-    if myanimelist_id is None:
-      log(f"Error: No mapping for anime '{show.title}' tvdbid: {tvdbid} season: {season}", Fore.RED)
-      utils.add_to_error_log(show.title, tvdbid, season)
+        # If the myanimelist id could not be found then it needs to logged so I can add the mapping manually
+        if myanimelist_id is None:
+            log(f"Error: No mapping for anime '{show.title}' tvdbid: {tvdbid} season: {season}", Fore.RED)
+            utils.add_to_error_log(show.title, tvdbid, season)
 
-    self.tvdb_myanimelist_mapping[tvdbid][season] = str(myanimelist_id)
+        self.tvdb_myanimelist_mapping[tvdbid][season] = str(myanimelist_id)
 
-  def update_tvdbid_myanimelist_mappings(self, driver):
-    """ Checks the tvdb id to myanimelist id mapping and create mappings for any unmapped ones.
+    def update_tvdbid_myanimelist_mappings(self, driver):
+        """ Checks the tvdb id to myanimelist id mapping and create mappings for any unmapped ones.
 
-    :param driver: A chromedriver to pass into the create_mapping function.
-    :return: New list of updated mappings.
-    """
-    for show in self.get_shows(LIBRARY):
-      # Extract the tvdbid
-      tvdbid = self.get_show_tvdbid(show)
-      season_numbers = [str(x.seasonNumber) for x in show.seasons() if 'Season' in x.title]
+        :param driver: A chromedriver to pass into the create_mapping function.
+        :return: New list of updated mappings.
+        """
+        for show in self.get_shows(LIBRARY):
+            # Extract the tvdbid
+            tvdbid = self.get_show_tvdbid(show)
+            season_numbers = [str(x.seasonNumber) for x in show.seasons() if 'Season' in x.title]
 
-      # If mapping isn't defined add a blank value
-      if not tvdbid in self.tvdb_myanimelist_mapping:
-        self.tvdb_myanimelist_mapping[tvdbid] = {}
+            # If mapping isn't defined add a blank value
+            if not tvdbid in self.tvdb_myanimelist_mapping:
+                self.tvdb_myanimelist_mapping[tvdbid] = {}
 
-      # Check if each season has a mapping
-      for season in season_numbers:
-        if self.tvdb_myanimelist_mapping[tvdbid].get(season) in [None, 'None']:
-          self.create_mapping(show, season, driver)
+            # Check if each season has a mapping
+            for season in season_numbers:
+                if self.tvdb_myanimelist_mapping[tvdbid].get(season) in [None, 'None']:
+                    self.create_mapping(show, season, driver)
 
-      # Save new mapping info
-      utils.save_json(self.tvdb_myanimelist_mapping, 'data/tvdb_myanimelist_mapping.json')
-    return self.tvdb_myanimelist_mapping
+            # Save new mapping info
+            utils.save_json(self.tvdb_myanimelist_mapping, 'data/tvdb_myanimelist_mapping.json')
+        return self.tvdb_myanimelist_mapping
--- utils.py
+++ utils.py
@@ -12,143 +12,143 @@ from config import MAPPING_AGE
 
 
 def log(text, *style):
-  """ Logs text with a specified style using colorama styles """
-  text = ((''.join(style) + text) + Style.RESET_ALL).replace('\r', '').replace('\n', '')
-  print(datetime.today().strftime('%d-%m-%Y %H:%M:%S') + ' ' + text)
+    """ Logs text with a specified style using colorama styles """
+    text = ((''.join(style) + text) + Style.RESET_ALL)
+    print(datetime.today().strftime('%d-%m-%Y %H:%M:%S') + ' ' + text)
 
-  # sys.stdout.write((''.join(style) + text).ljust(100) + Style.RESET_ALL)
-  # sys.stdout.flush()
+    # sys.stdout.write((''.join(style) + text).ljust(100) + Style.RESET_ALL)
+    # sys.stdout.flush()
 
 
 def load_json(filepath):
-  """ Opens json files and returns the parsed data.
+    """ Opens json files and returns the parsed data.
 
-  :param filepath: File path to the json file.
-  :return: Parsed json data file.
-  """
-  # Create file if it doesn't exist and return a blank dictionary
-  if not os.path.exists(filepath):
-    with open(filepath, 'w') as f:
-      f.write('{}')
-      return {}
+    :param filepath: File path to the json file.
+    :return: Parsed json data file.
+    """
+    # Create file if it doesn't exist and return a blank dictionary
+    if not os.path.exists(filepath):
+        with open(filepath, 'w') as f:
+            f.write('{}')
+            return {}
 
-  # Load from existing file
-  with open(filepath, 'r') as f:
-    return json.load(f)
+    # Load from existing file
+    with open(filepath, 'r') as f:
+        return json.load(f)
 
 
 def save_json(data, filepath):
-  """ Saved data to a json file at the specified file path
+    """ Saved data to a json file at the specified file path
 
-  :param data: The data to write to the json file.
-  :param filepath: The path where the json file will be saved.
-  """
-  with open(filepath, 'w') as f:
-    json.dump(data, f)
+    :param data: The data to write to the json file.
+    :param filepath: The path where the json file will be saved.
+    """
+    with open(filepath, 'w') as f:
+        json.dump(data, f)
 
 
 def get_watch_status(total_episodes, episodes_watched):
-  """ Decide what watch status an anime should have.
+    """ Decide what watch status an anime should have.
 
-  :param total_episodes: Number of total episodes in the anime.
-  :param episodes_watched: Number of episodes watched.
-  :return: String representing the watch status of the anime
-  """
-  status = 'To watch'
+    :param total_episodes: Number of total episodes in the anime.
+    :param episodes_watched: Number of episodes watched.
+    :return: String representing the watch status of the anime
+    """
+    status = 'To watch'
 
-  if episodes_watched != 0:
-    status = 'Watching'
+    if episodes_watched != 0:
+        status = 'Watching'
 
-  if total_episodes > 0 and total_episodes <= episodes_watched:
-    status = 'Completed'
+    if total_episodes > 0 and total_episodes <= episodes_watched:
+        status = 'Completed'
 
-  return status
+    return status
 
 
 def add_to_error_log(title, tvdbid, season):
-  """ Adds an entry to the error log to inform on any manual mappings that need to be done.
+    """ Adds an entry to the error log to inform on any manual mappings that need to be done.
 
-  :param title: The title of the anime causing the error.
-  :param tvdbid: The tvdb id for the anime causing the error.
-  :param season: The season number for the anime causing the error.
-  """
-  errors = load_json('data/errors.json')
-  if not tvdbid in errors:
-    errors[tvdbid] = {'title': title}
-  errors[tvdbid]['season'] = season
-  save_json(errors, 'data/errors.json')
+    :param title: The title of the anime causing the error.
+    :param tvdbid: The tvdb id for the anime causing the error.
+    :param season: The season number for the anime causing the error.
+    """
+    errors = load_json('data/errors.json')
+    if not tvdbid in errors:
+        errors[tvdbid] = {'title': title}
+    errors[tvdbid]['season'] = season
+    save_json(errors, 'data/errors.json')
 
 
 def get_tvdb_anidb_mapping():
-  """ Load the tvdb id to anidb id mapping xml file.
+    """ Load the tvdb id to anidb id mapping xml file.
 
-  If the xml file with the mapping is older than the specified date then it will be re-downloaded.
+    If the xml file with the mapping is older than the specified date then it will be re-downloaded.
 
-  :return: xml object containing the mapping data for the tvdb and anidb ids.
-  """
-  mapping_path = 'data/anime-list-full.xml'
+    :return: xml object containing the mapping data for the tvdb and anidb ids.
+    """
+    mapping_path = 'data/anime-list-full.xml'
 
-  if not os.path.exists(mapping_path):
-    download_tvdb_anidb_mapping()
+    if not os.path.exists(mapping_path):
+        download_tvdb_anidb_mapping()
 
-  creation_time = os.path.getctime(mapping_path)
-  age = time.time() - creation_time
-  if age >= MAPPING_AGE * 86400:
-    os.remove('data/anime-list-full.xml')
-    download_tvdb_anidb_mapping()
+    creation_time = os.path.getctime(mapping_path)
+    age = time.time() - creation_time
+    if age >= MAPPING_AGE * 86400:
+        os.remove('data/anime-list-full.xml')
+        download_tvdb_anidb_mapping()
 
-  return et.parse(mapping_path).getroot()
+    return et.parse(mapping_path).getroot()
 
 
 def download_tvdb_anidb_mapping():
-  """ Downloads the latest tvdb id to anidb id mapping file from the github repo. """
-  log('\nDownloading new tvdb myanimelist mapping file', Fore.CYAN)
-  urllib.request.urlretrieve('https://raw.githubusercontent.com/ScudLee/anime-lists/master/anime-list-full.xml',
-                             'data/anime-list-full.xml')
-  log('\rDownloaded new tvdb myanimelist mapping file', Fore.GREEN)
+    """ Downloads the latest tvdb id to anidb id mapping file from the github repo. """
+    log('\nDownloading new tvdb myanimelist mapping file', Fore.CYAN)
+    urllib.request.urlretrieve('https://raw.githubusercontent.com/ScudLee/anime-lists/master/anime-list-full.xml',
+                               'data/anime-list-full.xml')
+    log('\rDownloaded new tvdb myanimelist mapping file', Fore.GREEN)
 
 
 def get_myanimelist_id(anidb_id, driver):
-  """ Gets the myanimelist id from the anidb page for the anime.
+    """ Gets the myanimelist id from the anidb page for the anime.
 
-  :param anidb_id: The anidb id for the anime.
-  :param driver: A chromedriver for loading the anime page.
-  :return: Returns the myanimelist id as a string.
-  """
-  # When the anidb_id couldn't be found
-  if anidb_id == None:
-    return None
+    :param anidb_id: The anidb id for the anime.
+    :param driver: A chromedriver for loading the anime page.
+    :return: Returns the myanimelist id as a string.
+    """
+    # When the anidb_id couldn't be found
+    if anidb_id == None:
+        return None
 
-  url = 'https://anidb.net/perl-bin/animedb.pl?show=anime&aid=' + anidb_id
-  soup = BeautifulSoup(driver.get_html(url), 'lxml')
+    url = 'https://anidb.net/perl-bin/animedb.pl?show=anime&aid=' + anidb_id
+    soup = BeautifulSoup(driver.get_html(url), 'lxml')
 
-  # Check the two locations for the myanimelist link
-  ele = soup.find('a', {'class': 'i_icon i_resource_mal brand'})
-  if ele is None:
-    ele = soup.find('a', {'class': 'hide mal'})
+    # Check the two locations for the myanimelist link
+    ele = soup.find('a', {'class': 'i_icon i_resource_mal brand'})
+    if ele is None:
+        ele = soup.find('a', {'class': 'hide mal'})
 
-  return ele.get('href').lstrip('https://myanimelist.net/anime/')
+    return ele.get('href').lstrip('https://myanimelist.net/anime/')
 
 
 def find_anidb_id(tvdb_anidb_mapping, tvdbid, season):
-  """ Gets the anifb id from the tvdb id to anidb id mapping.
+    """ Gets the anifb id from the tvdb id to anidb id mapping.
 
-  :param tvdb_anidb_mapping: The mapping data for tvdb id to anidb id.
-  :param tvdbid: The tvdb id for the anime.
-  :param season: The season number for the anime being requested.
-  :return: The anidb id as a string or if it isn't found then None.
-  """
-  for type in tvdb_anidb_mapping.findall('anime'):
-    if type.get('tvdbid') == str(tvdbid) and type.get('defaulttvdbseason') == str(season):
-      return type.get('anidbid')
+    :param tvdb_anidb_mapping: The mapping data for tvdb id to anidb id.
+    :param tvdbid: The tvdb id for the anime.
+    :param season: The season number for the anime being requested.
+    :return: The anidb id as a string or if it isn't found then None.
+    """
+    for type in tvdb_anidb_mapping.findall('anime'):
+        if type.get('tvdbid') == str(tvdbid) and type.get('defaulttvdbseason') == str(season):
+            return type.get('anidbid')
 
-  return None
+    return None
 
 
 def get_mal_data_from_id(mal_id):
-  """ Gets anime data from myanimelist api
+    """ Gets anime data from myanimelist api
 
-  :param mal_id: The id of the anime to get data for.
-  :return: A dictionary containing information about the anime.
-  """
-  return requests.get(f'https://api.jikan.moe/v3/anime/{mal_id}/').json()
+    :param mal_id: The id of the anime to get data for.
+    :return: A dictionary containing information about the anime.
+    """
+    return requests.get(f'https://api.jikan.moe/v3/anime/{mal_id}/').json()
